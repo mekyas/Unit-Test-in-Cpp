92afb7e0113b7222bda65474335dedac4b76f73e
mekyas <mek.yas.91@gmail.com>
2021-11-17 13:45:22 +0900

    add boost test

---
+++ tests-factorial_boost_shared.cpp
@@ -0,0 +1,16 @@
+#define BOOST_TEST_MODULE factorial shared library test
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include "factorial.cpp"
+
+BOOST_AUTO_TEST_CASE(Factorials_for_zero) {
+    BOOST_TEST( Factorial(0) == 1 );
+}
+
+BOOST_AUTO_TEST_CASE(Factorials_for_positive_numbers) {
+    BOOST_TEST( Factorial(1) == 1 );
+    BOOST_TEST( Factorial(2) == 2 );
+    BOOST_TEST( Factorial(3) == 6 );
+    BOOST_TEST( Factorial(10) == 3628800 );
+}
---
+++ tests-factorial_boost_static.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_MODULE factorial static test
+#include <boost/test/unit_test.hpp>
+#include "factorial.cpp"
+
+BOOST_AUTO_TEST_CASE(Factorials_for_zero) {
+    BOOST_TEST( Factorial(0) == 1 );
+}
+
+BOOST_AUTO_TEST_CASE(Factorials_for_positive_numbers) {
+    BOOST_TEST( Factorial(1) == 1 );
+    BOOST_TEST( Factorial(2) == 2 );
+    BOOST_TEST( Factorial(3) == 6 );
+    BOOST_TEST( Factorial(10) == 3628800 );
+}
